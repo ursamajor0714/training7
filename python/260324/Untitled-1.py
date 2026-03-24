bdc4085767dfac1b904d2aa6862982d8db6e28db
ursamajor0714 <ohyou66@gmail.com>
2026-03-24 15:26:38 +0900

    ㄴㄴㄴ

---
+++ python/260324/Untitled-1.py
@@ -0,0 +1,18 @@
+
+latter = 'xxxxxxxxxxxxxxxxxxxx'
+print(latter.count('x'))
+
+s='나도고등학교'
+print(s.startswith('나도'))
+print(s.endswith('초등학교'))
+print(s.endswith('고등학교'))
+
+s2 = '...나도고등학교...'
+print(s2.strip('.'))
+
+
+s3 = '.,.나도고등학교.,.'
+print(s3.strip('.'))
+
+s4 = '나도고등학교'
+print(s4.replace('고등학교','고교'))
--- python/260324/view.py
+++ python/260324/view.py
@@ -36,8 +36,13 @@ print('c' not in 'cat')
 print("5 + 10 = ",(3*5))
 
 lang='PYTHON'
+print(lang)
 print(lang[0])
+print(lang[-1])
+print(lang[:3])
+print(lang[2:])
 print(lang[:])
+
 
 print("5 + 6 =", 5.5 * 2)
 print("10 - 3 =", 3.5 * 2)
@@ -54,3 +59,43 @@ print("hap",kor +mat + eng)
 print('-'*10)
 print('nadocoding')
 print('-'*10)
+
+
+
+
+name = "kim"
+age = 15
+
+print(name,"is",age,"years old.")
+
+
+num = 1
+
+num-=1
+
+snack = '꿀꽈배기'
+print(len(snack))
+
+
+Lee = "Your name is Lee"
+print(Lee)
+
+
+
+a = input()
+b = input()
+
+print("Capital Of",a,"is",b)
+
+
+
+x = float(input())
+y = float(input())
+
+print("좌표 =",(0<x,y<100))
+
+
+
+
+latter = 'xxxxxxxxxxxxxxxxxxxx'
+print(latter.upper())
