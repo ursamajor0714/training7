ad4fd93fa985e6c94f5c61e04632bf6ddec20b02
ursamajor0714 <ohyou66@gmail.com>
2026-03-24 12:14:46 +0900

    python

--- python/260324/view.py
+++ python/260324/view.py
@@ -2,3 +2,55 @@ print("""My age:
 15
 My weight:
 67.3""")
+
+print("Apple,Banana,Carrot")
+
+print("*   *")
+
+
+
+print("""*   *
+ * * 
+ *""")
+
+
+print("""(@) (@)
+( *~* )
+ (^^^)""")
+
+print(5>2)
+
+print(5%2)
+print(5 // 2)
+print(5 ** 2)
+
+print(5>2)
+print(5>=2)
+print(5<2)
+print(5<=2)
+
+
+print('c' in 'cat')
+print('c' not in 'cat')
+
+print("5 + 10 = ",(3*5))
+
+lang='PYTHON'
+print(lang[0])
+print(lang[:])
+
+print("5 + 6 =", 5.5 * 2)
+print("10 - 3 =", 3.5 * 2)
+
+
+kor = 90
+mat = 80
+eng = 100
+print("""kor 90
+mat 80
+eng 100""")
+print("hap",kor +mat + eng)
+
+print('-'*10)
+print('nadocoding')
+print('-'*10)
