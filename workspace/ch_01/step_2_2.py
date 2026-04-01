78cc9d5606d78ce7043f4e93243fbfc716ab4816
ursamajor0714 <ohyou66@gmail.com>
2026-04-01 18:19:51 +0900

    alonhpython

---
+++ workspace/ch_01/step_2_2.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from step_2_1 import WORK_DIR
+
+def ger_total_filesize(base_dir: Path, pattern: str = "*")-> int:
+    total_size = total_bytes = 0
+    for fullpath in base_dir.glob(pattern):
+        if fullpath.is_file():
+            total_bytes += fullpath.stat().st_size
+    return total_bytes
+
+if __name__ == "__main__":
+    base_dir = WORK_DIR
+    filesize = ger_total_filesize(base_dir, pattern="*")
+    print(f"{base_dir.as_posix()=}, {filesize=} bytes")
+
+
